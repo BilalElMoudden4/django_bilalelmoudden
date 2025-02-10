c5877a17550910c22f2381a45a83972b609f1fab
Bilal El Moudden <belel22@jaumebalmes.net>
2025-02-10 18:37:48 +0100

    Creación de la aplicación autenticacion

--- TIC_BCN_inicials/urls.py
+++ TIC_BCN_inicials/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('centre/', include('centre.urls')),
-    path('authentication/', include('authentication.urls')),
+    path('', include('autenticacion.urls')),
 ]
