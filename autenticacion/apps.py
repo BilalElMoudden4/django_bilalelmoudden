d51b3163774ef177fa003e089372c63fa2fa477b
Bilal El Moudden <belel22@jaumebalmes.net>
2025-02-10 18:37:48 +0100

    Creación de la aplicación autenticacion

--- autenticacion/apps.py
+++ autenticacion/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AuthenticationConfig(AppConfig):
+class AutenticacionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'authentication'
+    name = 'autenticacion'
